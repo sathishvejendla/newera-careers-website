fcf4fb0f184996368285eaca3c7ab095ccbe5cc9
sathishvejendla <153282599+sathishvejendla@users.noreply.github.com>
2025-02-18 11:46:38 +0000

    first webpage created

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask
-
+from flask import Flask, render_template, jsonify
+#flask= module, Flask= class
 app = Flask(__name__)
 
+JOBS=[
+    {
+        "id":1,
+        "title":"Data Analyst",
+        "location":"Bengaluru, India",
+        "salary":"Rs. 10,00,000"    
+    },
+    {
+        "id":2,
+        "title":"Data Scientist",
+        "location":"Hyderabad, India",
+        "salary":"Rs. 15,00,000"    
+    },
+    {
+        "id":3,
+        "title":"Front-End Developer",
+        "location":"Mumbai, India",
+           
+    },
+    {
+        "id":4,
+        "title":"Back-End Developer",
+        "location":"Pune, India",
+        "salary":"Rs. 12,00,000"    
+    }
+]
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("home.html",jobs=JOBS,company_name="NewEra")
+
+@app.route("/jobs")
+def list_jobs():
+    return jsonify(JOBS)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
